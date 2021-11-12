065cbc64c2722cdf04db1599c0dc6d045a02120b
Dominique Thomas <63202540+Dthomas0729@users.noreply.github.com>
2021-11-11 22:39:03 -0500

    ADD TopShelf image and edit background

--- src/components/TopShelf/index.js
+++ src/components/TopShelf/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './styles.css'
+import topShelfImg from '../../images/top-shelf.jpg';
 
 const TopShelf = () => {
     return (
         <section className="top-shelf">
+            <div style={{ backgroundImage: `url(${topShelfImg})` }} alt="top shelf" className="top-shelf-image"> </div>
             <h1>Top Shelf</h1>
 
         </section>
